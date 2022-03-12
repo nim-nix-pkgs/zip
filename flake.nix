d99637a86809434ca2a3b2272f534d9f7c7ae418
hugosenari <hugosenari@gmail.com>
2022-03-12 01:34:14 -0300

    chore: re index zip

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  description = ''A wrapper for the zip library'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs."zip-master".url = "path:./master";
+  inputs."zip-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."zip-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  inputs."zip-0_3_1".url = "path:./0_3_1";
+  inputs."zip-0_3_1".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."zip-0_3_1".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib"];
+  in lib.mkProjectOutput {
+    inherit self nixpkgs;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+    refs = builtins.removeAttrs inputs args;
+  };
+}
---
+++ master/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = ''A wrapper for the zip library'';
+
+  inputs.flakeNimbleLib.owner = "riinr";
+  inputs.flakeNimbleLib.ref   = "master";
+  inputs.flakeNimbleLib.repo  = "nim-flakes-lib";
+  inputs.flakeNimbleLib.type  = "github";
+  inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
+  
+  inputs.src-zip-master.flake = false;
+  inputs.src-zip-master.owner = "nim-lang";
+  inputs.src-zip-master.ref   = "refs/heads/master";
+  inputs.src-zip-master.repo  = "zip";
+  inputs.src-zip-master.type  = "github";
+  
+  outputs = { self, nixpkgs, flakeNimbleLib, ...}@deps:
+  let 
+    lib  = flakeNimbleLib.lib;
+    args = ["self" "nixpkgs" "flakeNimbleLib" "src-zip-master"];
+  in lib.mkRefOutput {
+    inherit self nixpkgs ;
+    src  = deps."src-zip-master";
+    deps = builtins.removeAttrs deps args;
+    meta = builtins.fromJSON (builtins.readFile ./meta.json);
+  };
+}
